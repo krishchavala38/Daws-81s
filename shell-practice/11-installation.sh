a32b90f960c9388abfdc3827ff3e3a1565157dc9
krish chavala <krishchavala@krishs-Mac-mini.local>
2024-09-20 16:06:37 +0530

    11 file completed

--- shell-practice/11-installation.sh
+++ shell-practice/11-installation.sh
@@ -10,3 +10,35 @@ then
 fi
 
 dnf list installed git # Just checking whether installed or not
+
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed, going to install it.."
+    dnf install git -y # here installing
+    if [ $? -ne 0 ]
+    then
+        echo "Git installation is not success...check it"
+        exit 1
+    else
+        echo "Git installation is success"
+    fi
+else
+    echo "Git is already installed, nothing to do.."
+fi
+
+dnf list installed mysql 
+
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed...going to install"
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "MySQL installation is failure..please check"
+        exit 1
+    else
+        echo "MySQL installation is success"
+    fi
+else
+    echo "MySQL is already installed..nothing to do"
+fi
