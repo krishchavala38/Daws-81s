7a0a6b85472928c5f3b2f74a9170ae0b9029002b
krish chavala <krishchavala@krishs-Mac-mini.local>
2024-09-20 15:52:46 +0530

    11 file added

--- shell-practice/11.installation.sh
+++ shell-practice/11.installation.sh
@@ -6,7 +6,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Please run this script with root priveleges"
-    
+    exit 1
 fi
 
 dnf list installed git # Just checking whether installed or not
