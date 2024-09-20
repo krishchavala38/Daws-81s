db71e03ea9a181ca3a7b09f201259312f8a4613a
krish chavala <krishchavala@krishs-Mac-mini.local>
2024-09-20 15:42:23 +0530

    11 file added

---
+++ shell-practice/11.installation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+USERID=$(id -u)
+#echo "User ID is: $USERID"
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root priveleges"
+    sudo su -
+fi
+
+dnf install git -y
